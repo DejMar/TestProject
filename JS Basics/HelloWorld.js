49e9e3912386934c02964f1b7d0f52c79effbea9
DejMar <marjanovic.dejan@gmail.com>
2018-08-31 22:18:49 +0200

    Peta promjena

--- JS Basics/HelloWorld.js
+++ JS Basics/HelloWorld.js
@@ -1,4 +1,5 @@
 console.log('Hello world');
 console.log('Hello new world');
 console.log('Hello for third time');
-console.log('Hello for forth time...')
+console.log('Hello for forth time...');
+console.log('Hello for fifth time...');
